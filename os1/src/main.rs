61e0ebcea496138c9c6a23fac19d87b6988027cc
long <zhanglong307@126.com>
2022-11-23 17:01:59 +0800

    first commit

--- os1/src/main.rs
+++ os1/src/main.rs
@@ -16,6 +16,7 @@ fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
+
     }
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
