b65fb59a52db3f6c05c9020160841147ef6f250e
xiu307 <383780369@qq.com>
2022-11-24 14:34:20 +0000

    update classroom.yml .keep autograding.json for classroom CI test

--- os1/src/main.rs
+++ os1/src/main.rs
@@ -16,7 +16,6 @@ fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
-
     }
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
